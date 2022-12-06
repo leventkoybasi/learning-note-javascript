fc21f675aaa3cfc2b020b98828e52dc36a78dcad
Levent KOYBASI <leventkoybasi@hotmail.com>
2022-12-05 23:55:03 -0800

    1

---
+++ Temeller/Console/app.js
@@ -0,0 +1,14 @@
+// console.log("Merhaba JavaScript");
+// console.log(213213);
+// console.log(true);
+// console.log(false);
+
+// var a = 20;
+// console.log(a);
+
+// console.log(["Ahmet", "Mehmet"]);
+// console.log(typeof a);
+
+console.warn("Bu bir uyaridir");
+console.error("Bu sayfa bulunamadi.");
+console.clear();
---
+++ Temeller/Degiskenler, VeriTipleri/app.js
@@ -0,0 +1,60 @@
+// Degisken Olusturma
+
+// var a = 20;
+// var b = 10;
+// var c = 40;
+
+// console.log(a, b, c);
+
+// Primitive
+var a = 10; //Number
+var b = 3.14; //Number
+console.log(a);
+console.log(typeof a);
+console.log(typeof b);
+
+var name = "Mustafa"; //String
+console.log(name);
+console.log(typeof name);
+
+var a = true; //Boolean
+console.log(a);
+
+var b = false; //Boolean
+console.log(b);
+console.log(typeof a);
+console.log(typeof b);
+
+var a = null; //bos null
+console.log(a);
+console.log(typeof a);
+
+var c;
+//c = 20; //Undefined (deger verilmemis)
+console.log(c);
+
+// Reference Veri Tipi
+
+var numbers = [1, 2, 3, 4, 5]; //Number
+console.log(numbers);
+console.log(typeof a);
+console.log(numbers[0]);
+console.log(numbers[4]);
+
+var person = {
+  name: "Levent KOYBASI",
+  age: 35,
+};
+console.log(person);
+console.log(typeof person);
+
+var date = new Date();
+console.log(date);
+console.log(typeof date);
+
+var merhaba = function () {
+  console.log("Merhaba");
+};
+
+console.log(merhaba);
+console.log(typeof merhaba);
