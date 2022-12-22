c83476c9e53c904ffb88ca305eb6b15333ae6687
Levent KOYBASI <leventkoybasi@hotmail.com>
2022-12-21 22:16:36 -0800

    Switch Case

---
+++ JS Temelleri Part 02/Switch Case/app.js
@@ -0,0 +1,15 @@
+const process = 4;
+
+switch (process) {
+  case 1:
+    console.log("Islem 1");
+    break;
+  case 2:
+    console.log("Islem 2");
+    break;
+  case 3:
+    console.log("Islem 3");
+    break;
+  default:
+    console.log("Gecersiz Islem");
+}
