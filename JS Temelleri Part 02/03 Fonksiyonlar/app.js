80593bd43b535977e63d24c32fbbe865d28fcb85
Lacrimossa <leventkoybasi@hotmail.com>
2022-12-26 13:52:46 -0800

    test

--- JS Temelleri Part 02/03 Fonksiyonlar/app.js
+++ JS Temelleri Part 02/03 Fonksiyonlar/app.js
@@ -1,7 +1,63 @@
 // Fonksiyon Tanimlama
 
-function merhaba(name, age) {
+function merhaba(name = "Bilgi YoK", age = "Bilgi YoK") {
+  // if (typeof name === "undefined") name = "Bilgi Yok";
+
+  // if (typeof age === "undefined") age = "Bilgi Yok ";
   console.log(`Isim: ${name} Yas: ${age}`);
 }
 
-merhaba("Levent", 37);
+merhaba("Levent", 37); //Fonksiyon Cagrisi function call
+
+merhaba("Ayse", 22);
+
+merhaba();
+
+merhaba("Seda");
+
+// Fonksiyonlarda Return
+
+function square(x) {
+  return x * x; //Return bize deger donmesini saglar
+
+  console.log("Naber"); //Return bize fonk bittigini soyler bu satir calismadigi icin naber yazisi ekrana yazdirilamaz.
+}
+function cube(x) {
+  return x * x * x;
+}
+
+// let a = square(12);
+// a = cube(a);
+
+// let a = cube(square(12));
+// console.log(a);
+
+// function merhaba() {
+//   return "merhaba";
+// }
+// console.log(merhaba());
+
+//Function Expression
+
+const merhabaa = function (name) {
+  console.log("Merhaba " + name);
+};
+
+merhabaa("Murat");
+
+//Immediately Invoke Function Expression (IIFE)
+
+(function (name) {
+  console.log("Merhaba " + name);
+})("Levent");
+
+const database = {
+  host: "localhost",
+  add: function () {
+    console.log("Eklendi");
+  },
+
+  get: function () {
+    console.log;
+  },
+};
