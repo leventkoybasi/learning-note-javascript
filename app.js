9730e474013406f2a90843c9fdcdecb22d612123
Lacrimossa <leventkoybasi@hotmail.com>
2022-12-27 17:48:33 -0800

    Window Objesi

---
+++ app.js
@@ -0,0 +1,47 @@
+// console.log(alert("Merhaba"));
+
+// alert("Merhaba");
+// console.log(cevap);
+
+//CONFIRM ISLEMI
+
+// const cevap = confirm("Emin misiniz?"); // true false doner
+
+// if (cevap == true) {
+//   console.log("Silin Gitsin");
+// } else {
+//   console.log("Silmeyin");
+// }
+
+// const cevap = prompt("2 + 2 = ?");
+
+// if (cevap == 4) {
+//   console.log("Dogru");
+// } else {
+//   console.log("Yanlis");
+// }
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+// if (confirm("Sayfa Yenilensin mi?")) {
+//   window.location.reload();
+// } else {
+//   console.log("Sayfa Yenilenmedi");
+// }
+
+value = window.outerHeight;
+value = window.outerWidth;
+value = window.innerHeight;
+value = window.innerWidth;
+
+value = window.scrollX;
+value = window.scrollY;
+
+console.log(value);
