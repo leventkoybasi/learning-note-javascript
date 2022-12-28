5d19808fab85ff44ea867a387743e5a02218a10e
Lacrimossa <leventkoybasi@hotmail.com>
2022-12-27 18:03:06 -0800

    Block scope

--- JS Temelleri Part 02/06 Scope Kavrami/app.js
+++ JS Temelleri Part 02/06 Scope Kavrami/app.js
@@ -1,1 +1,35 @@
-// 06 Scope Kavrami
+// 06 Scope Kavrami (Kapsam)
+
+// function a() {
+//   //function scope
+// }
+
+// if() {
+//     // Block scope
+// }
+
+//Global Scope
+
+var value1 = 10;
+let value2 = 20;
+const value3 = 30;
+
+// function func() {
+//   var value1 = 40;
+//   let value2 = 50;
+//   const value3 = 60;
+//   console.log(value1, value2, value3);
+// }
+// func();
+
+if (true) {
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  // console.log(a,b,c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+// console.log(value1, value2, value3);
