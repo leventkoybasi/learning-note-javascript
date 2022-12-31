39782509a273d9ccb70b9a2bc50c729cd6dce640
Lacrimossa <leventkoybasi@hotmail.com>
2022-12-30 17:28:24 -0800

    yeni

--- 03 JS Ile DOM Manipulasyonu/05 Elementleri Secme ve Style Ozelliklerini Degistirme/app.js
+++ 03 JS Ile DOM Manipulasyonu/05 Elementleri Secme ve Style Ozelliklerini Degistirme/app.js
@@ -1,6 +1,6 @@
 // 05 Elementleri Secme ve Style Ozelliklerini Degistirme
 
-const element = document.querySelector("#clear-todos");
+// const element = document.querySelector("#clear-todos");
 
 // console.log(element.id);
 // console.log(element);
@@ -14,10 +14,35 @@ const element = document.querySelector("#clear-todos");
 
 //Style ve Element Ozelliklerini Degistirme
 
-element.className = "btn btn-primary";
-element.style.color = "#000";
-element.style.marginLeft = "50px";
-element.href = "http://www.google.com.tr";
-element.target = "_blank";
+// element.className = "btn btn-primary";
+// element.style.color = "#000";
+// element.style.marginLeft = "50px";
+// element.href = "http://www.google.com.tr";
+// element.target = "_blank";
+// console.log(element.textContent);
+// element.textContent = "Silin";
+// element.textContent = "<span>Silin</sapn>"; //Span etiketi normal yazi gibi gorundu bi etiket olarak gorunmedi bunu engellemek icin inner html kullaniriz
 
-console.log(element.textContent);
+// element.innerHTML = "<span style = 'color: green'>Silin</span>";
+
+// const element = document.querySelectorAll(".list-group-item"); // node list
+
+// element.forEach(function (el) {
+//   el.style.color = "red";
+//   el.style.backgroundColor = "#eee";
+// });
+
+// let element2 = document.querySelector("li:first-child");
+let element2 = document.querySelector("li:last-child");
+element2 = document.querySelector("li:nth-child(2)");
+element2 = document.querySelector("li:nth-child(3)");
+element2 = document.querySelector("li:nth-child(4)");
+element2 = document.querySelectorAll("li:nth-child(odd)");
+element2 = document.querySelectorAll("li:nth-child(even)");
+
+element2.forEach(function (el) {
+  el.style.backgroundColor = "#ccc";
+  el.style.color = "red";
+});
+
+console.log(element2);
