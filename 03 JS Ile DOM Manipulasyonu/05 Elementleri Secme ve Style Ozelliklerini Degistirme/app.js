c070ca6dd9415326eeee8569457c7798509a60d5
Lacrimossa <leventkoybasi@hotmail.com>
2022-12-30 16:58:48 -0800

    eLEMENT SECME VE sTYLE kULLANIMI

--- 03 JS Ile DOM Manipulasyonu/05 Elementleri Secme ve Style Ozelliklerini Degistirme/app.js
+++ 03 JS Ile DOM Manipulasyonu/05 Elementleri Secme ve Style Ozelliklerini Degistirme/app.js
@@ -1,1 +1,23 @@
 // 05 Elementleri Secme ve Style Ozelliklerini Degistirme
+
+const element = document.querySelector("#clear-todos");
+
+// console.log(element.id);
+// console.log(element);
+// console.log(element.className);
+// console.log(element.classList);
+// console.log(element.classList[0]);
+// console.log(element.textContent);
+// console.log(element.innerHTML);
+// console.log(element.href);
+// console.log(element.style);
+
+//Style ve Element Ozelliklerini Degistirme
+
+element.className = "btn btn-primary";
+element.style.color = "#000";
+element.style.marginLeft = "50px";
+element.href = "http://www.google.com.tr";
+element.target = "_blank";
+
+console.log(element.textContent);
