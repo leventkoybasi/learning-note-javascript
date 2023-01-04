09d5b421725f10da4ffe5a26607a0e5f4d19d361
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-04 15:24:58 -0800

    app eklendi

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -1,1 +1,11 @@
 // Calisma Sayfasi
+
+// Window Object
+let value;
+value = document;
+value = document.all;
+value = document.all.length;
+value = document.all[0];
+value = document.all[6];
+
+console.log(value);
