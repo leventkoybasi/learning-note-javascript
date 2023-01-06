93c8eeab3bbae66eead28bb14837a91e269d8d56
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-01-06 14:54:09 -0800

    dinamik att eklendi

--- 03 JS Ile DOM Manipulasyonu/10 Dinamik Attribute Degistirme Silme Ekleme/app.js
+++ 03 JS Ile DOM Manipulasyonu/10 Dinamik Attribute Degistirme Silme Ekleme/app.js
@@ -1,1 +1,7 @@
 // 10 Dinamik Attribute Degistirme Silme Ekleme
+
+const newLink = document.createElement("a");
+const cardbody = document.getElementsByClassName("card-body")[1];
+
+console.log(newLink);
+console.log(cardbody);
