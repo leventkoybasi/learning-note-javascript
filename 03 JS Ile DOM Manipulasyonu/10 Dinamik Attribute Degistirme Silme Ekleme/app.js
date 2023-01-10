40ffed42269b578f3af19f4705586dbe2d8f6ad4
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-09 17:34:48 -0800

    Dinamik Attribute Degistirme

--- 03 JS Ile DOM Manipulasyonu/10 Dinamik Attribute Degistirme Silme Ekleme/app.js
+++ 03 JS Ile DOM Manipulasyonu/10 Dinamik Attribute Degistirme Silme Ekleme/app.js
@@ -1,7 +1,24 @@
 // 10 Dinamik Attribute Degistirme Silme Ekleme
 
-const newLink = document.createElement("a");
-const cardbody = document.getElementsByClassName("card-body")[1];
+const todoInput = document.getElementById("todo");
 
-console.log(newLink);
-console.log(cardbody);
+let element;
+
+element = todoInput;
+element = todoInput.classList;
+
+// todoInput.classList.add("newClass");
+// todoInput.classList.add("newClass2");
+// todoInput.classList.remove("form-control");
+
+element = todoInput;
+element = todoInput.placeholder;
+element = todoInput.getAttribute("placeholder");
+todoInput.setAttribute("placeholder", "Naber");
+todoInput.setAttribute("title", "Input");
+todoInput.removeAttribute("name");
+element = todoInput;
+
+// element = todoInput.hasAttribute("name");
+
+console.log(element);
