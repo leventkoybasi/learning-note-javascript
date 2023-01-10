b89852719b2b55343276a599d7a445f13af78412
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-09 16:35:43 -0800

    Dinamik ELemnt Silme

--- 03 JS Ile DOM Manipulasyonu/08 Dinamik Element Silme/app.js
+++ 03 JS Ile DOM Manipulasyonu/08 Dinamik Element Silme/app.js
@@ -1,1 +1,18 @@
 // 08 Dinamik Element Silme
+
+const todoList = document.querySelector("ul.list-group");
+const todos = document.querySelectorAll("li.list-group-item");
+
+//Remove Metodu
+
+// todos[0].remove();
+// todos[1].remove();
+
+//Remove Child
+
+// todoList.removeChild(todoList.lastElementChild);
+
+// todoList.removeChild(todos[3]);
+
+console.log(todos);
+console.log(todoList);
