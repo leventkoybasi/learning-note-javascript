d93f2f303d68cf5bbbc1d8e748feabe01ffa6bfe
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-09 17:58:32 -0800

    Window Object

--- 03 JS Ile DOM Manipulasyonu/02 Document Objesi - 1/app.js
+++ 03 JS Ile DOM Manipulasyonu/02 Document Objesi - 1/app.js
@@ -1,1 +1,22 @@
-// 02 Document Objesi - 1
+// Window Object
+
+let value;
+
+value = document;
+value = document.all;
+value = document.all.length;
+value = document.all[0];
+value = document.all[6];
+value = document.all[document.all.length - 1];
+
+const elements = document.all;
+
+// for (let i = 0; i < elements.length; i++) {
+//   console.log(elements[i]);
+// }
+
+// elements.forEach(function (elements) {
+//   console.log(elements);
+// }); //Kullanamayiz
+
+console.log(elements);
