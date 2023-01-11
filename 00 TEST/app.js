9e3435c6cfb051a5d6261f57b9a4573510947338
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-10 16:03:36 -0800

    ADD TO ELEMENT

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -1,12 +1,1 @@
-// Elementi ID ye gore secme
 
-let element;
-
-element = document.getElementById("todo-form");
-element = document.getElementById("tasks-title");
-
-// Elementi class a gore secme
-
-element = document.getElementsByClassName("list-group-item")[0];
-
-console.log(element);
