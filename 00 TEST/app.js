8e7e8630f9e983ccaed38da207f144067925dea3
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-10 18:14:25 -0800

    TEST APP

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -1,1 +1,58 @@
+// WHILE DONGULERI
 
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 10;
+
+// while (i > 0) {
+//   console.log(i);
+//   i -= 2;
+// }
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   if (i == 6) {
+//     break;
+//   }
+
+//   i++;
+// }
+
+//for
+
+const langs = ["python", "javascript", "Java"];
+// console.log(langs.length);
+// console.log(langs[2]);
+
+// let index = 0;
+
+// while (index < langs.length) {
+//   console.log(langs[index]);
+//   index++;
+// }
+
+// for (let index = 0; index < langs.length; index++) {
+//   console.log(langs[index]);
+// }
+
+// langs.forEach(function (lang, index) {
+//   console.log(lang, index);
+// });
+
+const users = [
+  { name: "John", age: 40 },
+  { name: "Ali", age: 30 },
+  { name: "Zeynep", age: 20 },
+];
+
+const names = users.map(function (user) {
+  return user.name;
+});
+console.log(names);
