336a93c394ae8b2d3f695706bc6d5853b1b71309
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-01-12 00:44:51 -0800

    Keyboard Eventleri

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -1,58 +1,8 @@
-// WHILE DONGULERI
+const a = document.getElementById("deneme");
+const b = document.getElementById("clear-todos");
 
-// let i = 0;
+b.addEventListener("click", writeText);
 
-// while (i < 10) {
-//   console.log(i);
-//   i++;
-// }
-
-// let i = 10;
-
-// while (i > 0) {
-//   console.log(i);
-//   i -= 2;
-// }
-
-// let i = 0;
-
-// while (i < 10) {
-//   console.log(i);
-//   if (i == 6) {
-//     break;
-//   }
-
-//   i++;
-// }
-
-//for
-
-const langs = ["python", "javascript", "Java"];
-// console.log(langs.length);
-// console.log(langs[2]);
-
-// let index = 0;
-
-// while (index < langs.length) {
-//   console.log(langs[index]);
-//   index++;
-// }
-
-// for (let index = 0; index < langs.length; index++) {
-//   console.log(langs[index]);
-// }
-
-// langs.forEach(function (lang, index) {
-//   console.log(lang, index);
-// });
-
-const users = [
-  { name: "John", age: 40 },
-  { name: "Ali", age: 30 },
-  { name: "Zeynep", age: 20 },
-];
-
-const names = users.map(function (user) {
-  return user.name;
-});
-console.log(names);
+function writeText() {
+  document.getElementById("deneme").innerHTML += " Levent";
+}
--- 04 Javascript DOM Elementleri/02 Keyboard Eventleri - KeyUp KeyPress KeyDown/app.js
+++ 04 Javascript DOM Elementleri/02 Keyboard Eventleri - KeyUp KeyPress KeyDown/app.js
@@ -1,1 +1,42 @@
 // 02 Keyboard Eventleri - KeyUp KeyPress KeyDown
+
+//Klavye Eventleri
+
+//Keypres (Klavyede harf ve sayilar eventi tetikler )
+// document.addEventListener("keypress", run);
+
+// function run(e) {
+//   console.log(e.which); //ASCII kodlari ile hangi rakam ve harfe basildigini gosterir ASCII kodlarinin tablosundan gorebiliriz buyuk kucuk harf duyarli ASCII tablosu 0'dan 250 dahil karakterleri numara olarka gosterir.
+//   console.log("Naber");
+//   console.log(e.key); // hangi tusa bastiysak onu direk gorebiliriz. Harf ve sayilar keypress eventini tetikler space vs tetiklemez.
+//}
+
+//Keydown (bu eventte ok tuslari tab tusu vs dahil tum karakterleri icerir)
+// document.addEventListener("keydown", run);
+
+// function run(e) {
+//   console.log(e.which);
+//   console.log("Naber");
+//   console.log(e.key);
+// }
+
+//Keyup (Tusa bastigimizde degil elimizi cektigimizde event tetiklenir tum karakterler dahildir.)
+
+// document.addEventListener("keyup", run);
+
+// function run(e) {
+//   console.log(e.which);
+//   console.log("Naber");
+//   console.log(e.key);
+// }
+
+const header = document.querySelector(".card-header");
+
+const todoInput = document.querySelector("#todo");
+
+todoInput.addEventListener("keyup", changeText);
+
+function changeText(e) {
+  header.textContent = e.target.value;
+  // console.log(e.target.value);
+}
