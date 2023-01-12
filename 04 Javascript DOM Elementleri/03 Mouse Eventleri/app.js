8213e3630f4c0ac672a7ec6c5575b8e10fd36411
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-12 15:30:02 -0800

    Mouse Eventleri

--- 04 Javascript DOM Elementleri/03 Mouse Eventleri/app.js
+++ 04 Javascript DOM Elementleri/03 Mouse Eventleri/app.js
@@ -1,1 +1,66 @@
 //03 Mouse Eventleri
+
+const cardBody = document.querySelectorAll(".card-body")[1];
+
+const title = document.querySelector("#tasks-title");
+
+// console.log(cardBody);
+// console.log(title); //Secimlerimizin dogru oldugunu kontrol ettik
+
+// // Click Eventi
+
+// title.addEventListener("click", run); //Mouse click event
+// title.addEventListener("dblclick", run); //Mouse Double click event
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+//Mouse Down //Mouse tusuna bastigimiz zaman event gerceklesir
+
+// title.addEventListener("mousedown", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+//Mouse Up //Mouse tusuna bastip elimizi cektigimiz zaman event gerceklesir
+
+// title.addEventListener("mouseup", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+//Mouse Over //Mouse eventi olusturan objenin uzerine geldigi zaman event gerceklesir
+
+// title.addEventListener("mouseover", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+//Mouse Out //Mouse eventi olusturan objenin uzerinden cikildigi zaman event gerceklesir
+
+// title.addEventListener("mouseout", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+// //Mouse Over //Mouse eventi cardBody classinda her bir element uzerinde gezdikce mouse over eventi olusur.
+
+// cardBody.addEventListener("mouseover", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
+
+//Mouse Enter ve Mouse Leave //Mouse Enter cardBody yani secili elemente girildiginde otomatik calisir, mouse leave ise bu elementten cikica calisir.
+
+// cardBody.addEventListener("mouseenter", run);
+// cardBody.addEventListener("mouseleave", run);
+
+// function run(e) {
+//   console.log(e.type);
+// }
