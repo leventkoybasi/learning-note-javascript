aad9ff64af445bee99cb1d04ca237fe130a6f34f
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-12 18:03:57 -0800

    Session Storage

--- 05 Tarayicilarin Depolama Alanlari/01 Session Storage/app.js
+++ 05 Tarayicilarin Depolama Alanlari/01 Session Storage/app.js
@@ -1,1 +1,33 @@
 // Session Storage - Key ve Value
+
+//Butonlari Secmek
+
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
+
+//Inputlari Secme
+
+const addkey = document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
+
+//Eventleri Yakalama
+
+add.addEventListener("click", addItem);
+del.addEventListener("click", deleteItem);
+clear.addEventListener("click", clearItem);
+
+//Function
+
+function addItem(e) {
+  sessionStorage.setItem(addkey.value, addvalue.value);
+}
+
+function deleteItem(e) {
+  sessionStorage.removeItem(deletekey.value);
+}
+
+function clearItem(e) {
+  sessionStorage.clear();
+}
