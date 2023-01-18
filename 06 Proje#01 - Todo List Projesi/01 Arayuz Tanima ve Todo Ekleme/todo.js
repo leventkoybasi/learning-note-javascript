6e48033b2e2ca69a14a32871f295709bc7602073
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-01-17 18:02:25 -0800

    selectorler eklendi

--- 06 Proje#01 - Todo List Projesi/01 Arayuz Tanima ve Todo Ekleme/todo.js
+++ 06 Proje#01 - Todo List Projesi/01 Arayuz Tanima ve Todo Ekleme/todo.js
@@ -1,3 +1,6 @@
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
