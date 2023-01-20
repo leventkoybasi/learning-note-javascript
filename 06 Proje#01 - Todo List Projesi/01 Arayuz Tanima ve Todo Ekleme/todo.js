a306714260cbc7c573e72b88f068e599b50c20c0
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-01-20 15:30:17 -0800

    bug ayiklandi

--- 06 Proje#01 - Todo List Projesi/01 Arayuz Tanima ve Todo Ekleme/todo.js
+++ 06 Proje#01 - Todo List Projesi/01 Arayuz Tanima ve Todo Ekleme/todo.js
@@ -1,6 +1,3 @@
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
-const form = document.querySelector("#todo-form");
-const todoInput = document.querySelector("#todo");
-const todoList = document.querySelector(".list-group");
