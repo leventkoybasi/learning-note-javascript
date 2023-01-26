11f1c58485e45489ab01abaa2eedc4389d4c265c
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-01-25 22:23:32 -0800

    112

--- 00 TEST/app.js
+++
@@ -1,8 +0,0 @@
-let greeting = document.querySelector("#greeting");
-greeting.classList.add("text-primary");
-greeting.classList.add("title");
-
-greeting.classList.remove("title");
-greeting.id = "levent";
-
-console.log(greeting.id);
