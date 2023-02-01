b17e6ebfce3a8ccc19fecb8ac22c25491993af7b
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-31 17:28:21 -0800

    INDEX HTML OLUSTURULKDU

--- 000 KODLUYORUZ/app.js
+++ 000 KODLUYORUZ/app.js
@@ -1,22 +1,13 @@
-let email = "leventkoybasi@hotmail.com";
-let firstName = "Levent";
-let lastName = "KOYBASI";
+console.log(document);
+document.write("Levent KOYBASI");
+console.log(document.body);
+console.log(document.location);
+console.log(document.location.pathname);
+document.body.style.color = "blue";
+document.body.style.background = "#ccc";
 
-console.log(`${email} ${email.length} Karakter uzunlugundadir.`);
-console.log(firstName[0]);
-console.log(firstName.toUpperCase());
-console.log(firstName.toLowerCase());
-
-console.log(email.search("@"));
-console.log(email[13]);
-console.log(email.slice(0, 13));
-console.log(email.slice(13));
-
-const DOMAIN = email.slice(email.search("@") + 1);
-console.log(DOMAIN);
-console.log(email.slice(email.search("@") + 1));
-
-console.log(email.slice(0, email.search("@")));
-
-console.log(DOMAIN);
-console.log(DOMAIN.slice(0, email.search("c") - 3));
+let value = document.getElementById("title");
+value.innerHTML = "Merhaba Dunya";
+value.style.color = "red";
+console.log(value);
+alert(value);
