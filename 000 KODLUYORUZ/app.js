93e48d9ced770f5e7a91e03b441d212ddfe74754
Lacrimossa <leventkoybasi@hotmail.com>
2023-01-31 18:04:45 -0800

    new item

--- 000 KODLUYORUZ/app.js
+++ 000 KODLUYORUZ/app.js
@@ -1,3 +1,4 @@
+/*
 let email = "leventkoybasi@hotmail.com";
 let firstName = "Levent";
 let lastName = "KOYBASI";
@@ -20,3 +21,25 @@ console.log(email.slice(0, email.search("@")));
 
 console.log(DOMAIN);
 console.log(DOMAIN.slice(0, email.search("c") - 3));
+
+*/
+
+let element = document.querySelector("#title");
+
+element.style.color = "red";
+
+console.log(element);
+title.innerHTML = "Degisen Bilgi";
+console.log(title.innerHTML);
+
+let link = document.querySelector("ul#list>li>a");
+link.innerHTML = "Link Bilgisi Degisti";
+link.style.color = "blue";
+link.classList.add("btn");
+// https://www.w3schools.com/jsref/dom_obj_style.asp //HTML DOM Style Object
+// link.style.textDecoration = "none";
+link.style.textDecorationColor = "red";
+console.log(link);
+
+let fullName = prompt("Adinizi Giriniz...");
+console.log(fullName);
