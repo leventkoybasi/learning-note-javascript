3dd600075bd53bd1eadc3b1a35d3568df952087b
Lacrimossa <leventkoybasi@hotmail.com>
2023-02-28 18:00:13 -0800

    Let ve Cons ile Degisken Tanimlama

---
+++ 01 KODLUYORUZ/06 - Degisken Turunu Kontrol Etmek ve Degisken Turunu Degistirmek/app.js
@@ -0,0 +1,35 @@
+// Let ve Cons ile Degisken Tanimlama
+
+// var ile degisken tanimlama
+
+var serverName = "api.kodluyoruz.org";
+
+console.log(serverName);
+
+// let ile degiskeni bos tanimlamak
+
+let serverName2;
+console.log(serverName2);
+
+//let ile degiskene bilgi aktarmak
+serverName2 = "https://kodluyoruz.org";
+console.log(serverName2);
+
+// let ile degiskene bilgi atayarak tanimlamak
+
+let password = "12345";
+console.log(password);
+
+// //degisken atamasi yapmadan once kullanmaya calsimak
+// console.log(fullName);
+// let fullName = "Levent KOYBASI"; //Hatali Kullanim
+
+let fullName2 = "Levent KOYBASI";
+console.log(fullName2);
+
+// const ile degiskeni bos tanimlamaya calsimak
+
+let fullName3 = "Lorem Ipsum Dolor";
+console.log(fullName3);
+
+// const ile degisken tanimlamak
