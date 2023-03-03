a5a0ece6d5be0cdb286da1073694e1bd38aa1677
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-03-03 00:14:49 -0800

    17 - Ternary Operator

--- 01 KODLUYORUZ/12 - Liste içerisindeki Öğeye Erişmek ve Yeni Öğe Eklemek/app.js
+++ 01 KODLUYORUZ/12 - Liste içerisindeki Öğeye Erişmek ve Yeni Öğe Eklemek/app.js
@@ -1,1 +1,13 @@
 // 12 - Liste içerisindeki Öğeye Erişmek ve Yeni Öğe Eklemek
+
+let lastChild = document.querySelector("ul#list>li:last-child");
+lastChild.innerHTML = "Son Oge Deisti";
+
+let firstChild = document.querySelector("ul#list>li:first-child");
+firstChild.innerHTML = "Ilk Oge Deisti";
+
+let newItem = document.createElement("li");
+newItem.innerHTML = "Bizim Ekledigimiz Oge";
+let ulDom = document.querySelector("#list");
+ulDom.appendChild(newItem); // en sona ekledi
+ulDom.prepend(newItem); // ilk oge olarak ekledi
