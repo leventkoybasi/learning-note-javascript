da54a6888f03e6358ea07229906afb293a7922e2
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-02 17:57:01 -0800

    prompt ile Kullanıcıdan Bilgi Almak

--- 01 KODLUYORUZ/06 - Degisken Turunu Kontrol Etmek ve Degisken Turunu Degistirmek/app.js
+++ 01 KODLUYORUZ/06 - Degisken Turunu Kontrol Etmek ve Degisken Turunu Degistirmek/app.js
@@ -1,35 +1,32 @@
-// Let ve Cons ile Degisken Tanimlama
+//06 - Degisken Turunu Kontrol Etmek ve Degisken Turunu Degistirmek
 
-// var ile degisken tanimlama
+//veri turunu ogrenmek typeof:
 
-var serverName = "api.kodluyoruz.org";
+let price = 111;
+let stringProce = "111";
+let hasPassword = true;
 
-console.log(serverName);
+console.log("Price: ", typeof price); // number
+console.log("stringProce: ", typeof stringProce); //string
+console.log("hasPassword: ", typeof hasPassword); //boolean
 
-// let ile degiskeni bos tanimlamak
+// string(metinsel) bilgileri int ve float'a donusturmek
+let number1 = "11";
+console.log("number1: ", parseInt(number1), typeof parseInt(number1)); //11 rakama cevrildi
 
-let serverName2;
-console.log(serverName2);
+let number2 = "11px";
+console.log("number2: ", parseInt(number2), typeof parseInt(number2)); //11 parseInt() ile icerisindeki rkamsal int bilgiyi alabiliyoruz
 
-//let ile degiskene bilgi aktarmak
-serverName2 = "https://kodluyoruz.org";
-console.log(serverName2);
+let number3 = "px11px";
+console.log("number3: ", parseInt(number3), typeof parseInt(number3)); //NaN //İlk karakter dönüştürülemezse, NaN döndürülür. bu yuzden ilk karakter rakam olmalidir.
 
-// let ile degiskene bilgi atayarak tanimlamak
+let number4 = "11px";
+console.log("number4: ", Number(number4), typeof Number(number4)); //NaN // Parse icerisinden rakamsal olarak bakip ve rakamsal basliyorsa veriyi sayiya ceviriyor ama number icerisinde sayi varsa ve baska karakter yoksa ceviriyor. Number ondalik sayilarda da doner.
 
-let password = "12345";
-console.log(password);
+let number5 = "11.4px";
+console.log("number5: ", parseFloat(number5), typeof parseFloat(number5)); //Icerisinde hem yazi hemde ondalik sayi varsa bu veri turunu Number() ile alamyacagimiz icin parseFloat() ile aliriz. Bunu parseInt ile almaya kalkarsak ondalik kismini alamayiz. Bize sadece 11 donder.
 
-// //degisken atamasi yapmadan once kullanmaya calsimak
-// console.log(fullName);
-// let fullName = "Levent KOYBASI"; //Hatali Kullanim
-
-let fullName2 = "Levent KOYBASI";
-console.log(fullName2);
-
-// const ile degiskeni bos tanimlamaya calsimak
-
-let fullName3 = "Lorem Ipsum Dolor";
-console.log(fullName3);
-
-// const ile degisken tanimlamak
+// number veri tipinden string'e donusturmek
+let number6 = 55;
+number6 = number6.toString();
+console.log(number6, typeof number6); // 55 string metin olarak doner
