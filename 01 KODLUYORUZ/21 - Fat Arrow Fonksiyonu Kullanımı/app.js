4c8190f734f5fcc0bebe5f5c25a9190f8b86db71
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-07 13:58:42 -0800

    Fat Arrow Fonksiyonu Kullanımı

--- 01 KODLUYORUZ/21 - Fat Arrow Fonksiyonu Kullanımı/app.js
+++ 01 KODLUYORUZ/21 - Fat Arrow Fonksiyonu Kullanımı/app.js
@@ -1,1 +1,29 @@
 // 21 - Fat Arrow Fonksiyonu Kullanımı
+
+// const degiskenIsmi = (parametre) => {RETURN}
+
+function hello(firstName) {
+	console.log(`Merhaba ${firstName}`);
+}
+
+hello("JavaScript");
+
+const helloFUnkV1 = (firstName) => {
+	console.log(`Merhaba ${firstName}`);
+};
+helloFUnkV1("helloFUnkV1");
+
+const helloFUnkV2 = (firstName) => console.log(`Merhaba ${firstName}`); // Eger tek bir paramatre var ise parametreyi parantez icinde yazmak sorunda degiliz ve eger tek bir cikti var ise ciktiyi suslu parantez ile yazmak zorunda degiliz
+helloFUnkV2("helloFUnkV2");
+
+const helloFUnkV3 = (firstName, lastName) =>
+	console.log(`Merhaba ${firstName}, ${lastName}`);
+helloFUnkV3("firstName", "lastName");
+
+const helloFUnkV4 = (firstName, lastName) => {
+	let info = `Merhaba ${firstName}, ${lastName}`;
+	console.log(info);
+	return info;
+};
+
+helloFUnkV4("firstName2", "lastName2");
