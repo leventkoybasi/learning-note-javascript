57353c9e841f5444cf96b4c1ada596b379f6bee5
Levent KOYBASI <leventkoybasi@hotmail.com>
2023-03-13 23:28:49 -0700

    112

--- 01 KODLUYORUZ/28 - Input İçerisinden Değer Almak/app.js
+++ 01 KODLUYORUZ/28 - Input İçerisinden Değer Almak/app.js
@@ -3,6 +3,8 @@ let formDOM = document.querySelector("#userForm");
 formDOM.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
-	event.preventDefault();
+	event.preventDefault(); //default islemini engelledik
 	console.log("Islem Gerceklesti");
+	let scoreInputDOM = document.querySelector("#score");
+	console.log(scoreInputDOM);
 }
