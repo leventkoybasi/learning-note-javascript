1bef32c3baa58703eba1bad1d66b26a0eecc2ec3
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-22 17:45:48 -0700

    Input İçerisinden Değer Almak

--- 01 KODLUYORUZ/27 - Form ve Form Submit Yönetimi/app.js
+++ 01 KODLUYORUZ/27 - Form ve Form Submit Yönetimi/app.js
@@ -1,1 +1,9 @@
 // 27 - Form ve Form Submit Yönetimi
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+	event.preventDefault(); //default islemi engelledik
+
+	console.log("ISLEM GERCEKLESTI");
+}
