110aae37822174a77e0a71b23a16857c230a93cc
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-24 13:10:15 -0700

    Bölüm Sonu Egzersizi

--- 01 KODLUYORUZ/29 - Formlarla Çalışmak  Bölüm Sonu Egzersizi/app.js
+++ 01 KODLUYORUZ/29 - Formlarla Çalışmak  Bölüm Sonu Egzersizi/app.js
@@ -9,12 +9,32 @@
 
 let userFormDOM = document.querySelector("#userForm");
 userFormDOM.addEventListener("submit", formHandler);
-
+const alertDOM = document.querySelector("#alert");
+const alertFunction = (
+	title,
+	message,
+	className = "waening"
+) => ` <div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong>${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div> `;
 function formHandler(event) {
 	event.preventDefault();
 	const USER_NAME = document.querySelector("#username");
 	const SCORE = document.querySelector("#score");
-	addItem(USER_NAME.value, SCORE.value);
+	if (USER_NAME.value && SCORE.value) {
+		addItem(USER_NAME.value, SCORE.value);
+		USER_NAME.value = ""; //Gonderdikten sonra sifirladik
+		SCORE.value = ""; //Gonderdikten sonra sifirladik
+		SCORE.value = "";
+	} else {
+		console.log("Hatali Giris");
+		alertDOM.innerHTML = alertFunction(
+			"Baslik Bilgisi",
+			"Eksik Bilgi Girdiniz.",
+			"danger"
+		);
+	}
 }
 /*  
 <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -27,12 +47,12 @@ let userListDOM = document.querySelector("#userList");
 
 const addItem = (userName, score) => {
 	let liDOM = document.createElement("li");
-	liDOM.innerHTML = `${username} ${score}`;
+	liDOM.innerHTML = `${userName} <span class="badge bg-primary rounded-pill">${score}</span>`;
 	liDOM.classList.add(
 		"list-group-item",
 		"d-flex",
 		"justify-content-between",
 		"align-items-center"
 	);
-	userListDOM.appendChild(liDOM);
+	userListDOM.append(liDOM);
 };
