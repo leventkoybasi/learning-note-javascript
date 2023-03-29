9b57c415e4ab48ae4180e67ad4fdf7471013cbc1
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-28 17:39:49 -0700

    Bölüm Sonu Egzersizi

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -1,68 +1,26 @@
-// Number
+// 29 - Formlarla Çalışmak  Bölüm Sonu Egzersizi
 
-// Number Veri Turunu Tanimla
-let price = 100;
-let tax = 0.18;
-let priceTax = price * tax;
-let total = price + priceTax;
-let discount = total - Math.round((total / 100) * 5);
-let stringNumber = "11";
+/*
+1 - Formu Sec
+2 - Input Bilgisini Bul Icine Ekle
+3 - Form Icindeki Bilgiyi Sifirla
+4 - Eger Forma Bilgi Girilmez ise Kullaniciyi Uyar
+*/
 
-console.log(
-  "Fiyat: ",
-  price,
-  "KDV Orani: ",
-  tax,
-  "KDV Tutari: ",
-  priceTax,
-  "Fiyat:  ",
-  total,
-  "Indirim: ",
-  discount
-);
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formHandler);
 
-//arttirma ve azaltma islemleri
+function formHandler(event) {
+    event.preventDefault();
+    const username = document.querySelector("#username");
+    const score = document.querySelector("#score");
 
-let counter = 320;
-counter = counter + 1; // uzun yontem
-counter += 1;
-counter++;
+console.log(username.value, score.value);
+}
 
-counter -= 1;
-counter--;
-
-counter *= 10;
-counter /= 2;
-
-console.log(counter);
-
-// islem onceligi
-
-console.log(2 + 3 * 10); // cikti 32
-console.log((2 + 3) * 10); //cikti 50
-
-// mod alma (kalan nedir)
-// sayi tek mi cift mi
-console.log(3 % 2); // cikti 1 //eger sifir ise cift 1 ise tek sayi
-
-// 8 urun alan kolide kac adet kaldi
-console.log(8 % 8); // cikti 0 // 8lik kolide 8 urun koyarsak elimizde urun kalir mi hayir
-
-console.log(18 % 8); // cikti 2 // 8lik kolide 2 urun elimizde kaldi
-
-//ust alma
-console.log(2 * 2 * 2 * 2); //cikti 16
-console.log(2 ** 4); //cikti 16 // ust alma islemi 2 carpi "**" ile yapilir
-
-//yuvarlam islemi
-
-console.log("Asagi Yuvarlama; ", Math.floor(1.9)); // cikti 1
-console.log("Yukari Yuvarlama; ", Math.ceil(1.9)); // cikti 2
-
-// yakina yuvarlama islemi
-console.log("Yakina Yuvarlama; ", Math.round(1.6)); // cikti 2
-console.log("Yakina Yuvarlama; ", Math.round(1.4)); // cikti 1
-
-// Number() kullanimi //string bir rakami number olark kullanma
-console.log(stringNumber); //cikti 11 ama string
-console.log(Number(stringNumber)); //cikti 11 ama number // Number() ile string bir rakami yine number olarak alabiliyoruz.
+/*  
+<li class="list-group-item d-flex justify-content-between align-items-center">
+A list item
+<span class="badge bg-primary rounded-pill"> 14 </span>
+</li>
+*/
