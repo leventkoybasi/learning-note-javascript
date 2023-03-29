fde1f477dface4c03755309a2a8f28b21215a580
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-28 18:14:47 -0700

    Bölüm Sonu Egzersizi2

--- 00 TEST/app.js
+++ 00 TEST/app.js
@@ -9,18 +9,49 @@
 
 let formDOM = document.querySelector("#userForm");
 formDOM.addEventListener("submit", formHandler);
+const alertDOM = document.querySelector("#alert");
+
+const alertFunction = (
+    title,
+    message,
+    className = "warning"
+) => `<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
 
 function formHandler(event) {
     event.preventDefault();
     const username = document.querySelector("#username");
     const score = document.querySelector("#score");
-
-console.log(username.value, score.value);
+    if (username.value && score.value) {
+        addItem(username.value, score.value);
+        username.value = ""; //Gonderdikten sonra sifirladik
+        score.value = "";
+    } else {
+        alertDOM.innerHTML = alertFunction(
+            "Noluyoo Lan",
+            "Eksik Bilgi Girdiniz!",
+            "success"
+        );
+    }
 }
 
+let userListDOM = document.querySelector("#userList");
+const addItem = (username, score) => {
+    let liDOM = document.createElement("li");
+    liDOM.innerHTML = `${username} <span class="badge bg-primary rounded-pill">${score}</span>`;
+    liDOM.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center",
+        "rounded-pill"
+    );
+    userListDOM.appendChild(liDOM);
+};
+
 /*  
-<li class="list-group-item d-flex justify-content-between align-items-center">
-A list item
-<span class="badge bg-primary rounded-pill"> 14 </span>
+<li class="list-group-item d-flex justify-content-between align-items-center rounded-pill"> Seda <span class="badge bg-primary rounded-pill">12</span>
 </li>
 */
