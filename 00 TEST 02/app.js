e41ded01cef56ab67ef6f846a22e5df6b5f83577
LEVENT_KOYBASI <leventkoybasi@hotmail.com>
2023-03-28 23:08:02 -0700

    ders2

--- 00 TEST 02/app.js
+++ 00 TEST 02/app.js
@@ -1,17 +1,52 @@
-/* 
-    $('div').mouseup(function() {
-        alert(getSelectedText());
-    });
+// 29 - Formlarla Çalışmak  Bölüm Sonu Egzersizi
 
-    function getSelectedText() {
-        if (window.getSelection) {
-            return window.getSelection().toString();
-        } else if (document.selection) {
-            return document.selection.createRange().text;
-        }
-        return '';
+/*
+1 - Formu Sec
+2 - Input Bilgisini Bul Icine Ekle
+3 - Form Icindeki Bilgiyi Sifirla
+4 - Eger Forma Bilgi Girilmez ise Kullaniciyi Uyar
+*/
+
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formHandler);
+const alertDOM = document.querySelector("#alert");
+
+const alertFunction = (
+    title,
+    message,
+    className = "warning"
+) => `<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
+
+function formHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#username");
+    const score = document.querySelector("#score");
+    if (username.value && score.value) {
+        addItem(username.value, score.value);
+        (username.value = ""), (score.value = "");
+    } else {
+        alertDOM.innerHTML = alertFunction(
+            "Noluyoo Lan",
+            "Eksik Bilgi Girdiniz!",
+            "success"
+        );
     }
-    */
-// http://jsfiddle.net/FvnPS/11/
+}
 
-// Proje Bir Metin olustur. Ustte tuslar ile o metindeki secilen yerleri tus islevleri ile duzenle. text edit programi gibi
+let userListDOM = document.querySelector("#userList");
+const addItem = (username, score) => {
+    let liDOM = document.createElement("li");
+    liDOM.innerHTML = `${username} <span class="badge bg-primary rounded-pill">${score}</span>`;
+    liDOM.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center",
+        "rounded-pill"
+    );
+    userListDOM.appendChild(liDOM);
+};
