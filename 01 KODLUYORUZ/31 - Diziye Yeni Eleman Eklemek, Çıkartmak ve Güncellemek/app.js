8cbb1cd03389cef3fa1aeaab171c0d6a51e2f148
Lacrimossa <leventkoybasi@hotmail.com>
2023-03-29 18:03:27 -0700

    Diziye Yeni Eleman Eklemk, Çıkartmak ve Güncellemk

--- 01 KODLUYORUZ/31 - Diziye Yeni Eleman Eklemek, Çıkartmak ve Güncellemek/app.js
+++ 01 KODLUYORUZ/31 - Diziye Yeni Eleman Eklemek, Çıkartmak ve Güncellemek/app.js
@@ -1,27 +1,32 @@
 // 31 - Diziye Yeni Eleman Eklemek, Çıkartmak ve Güncellemek
 
-// array olusturmak
-let domain = "kodluyoruz";
-let isActive = false;
-let items = [15, 25, 35, isActive, domain];
+let items = [10, 20, 30, 40];
+console.log("items - ilk hali : ", items);
+
+// Array ; sona oge/eleman ekleme - push
+items.push(50);
+console.log("50 : ", items);
+
+// Array ; basa oge/eleman ekleme - unshift
+items.unshift(5);
+console.log("5 : ", items);
+
+// Array ; sondaki oge/eleman cikartmak - pop
+let lastItem = items.pop(); //son elemani lastItem icerisine ekledik ve cikardik // sadece items.pop() ile de son ogeyi cikarta biliyoruz fakat bir degiskene baglamak ileride isimize yarayabilir bu sekil kullan
+console.log("lastItem : ", lastItem, "items : ", items);
+
+// Array ; bastaki oge/eleman cikartmak - shift
+let firstItem = items.shift();
+console.log("firstItem : ", firstItem, "items : ", items);
+
+//Array icerisindeki bir ogenin bilgisinin degistirilmesi:
+// ilk oge/eleman degistirme
+items[0] = 5;
 console.log(items);
 
-let emtyArray = [];
-// array icerisindeki eleman/oge sayisini ogrenmek
-console.log(items.length);
-// document.querySelector("#info").innerHTML = items.length;
+// son oge/eleman degistirme
+items[items.length - 1] = 60;
+console.log(items);
 
-// array icerisindeki ilk elemanin cagrilmasi
-console.log(items[0]);
-// array icerisindeki son elemanin cagrilmasi
-console.log(items[items.length - 1]);
-// array icerisindeki ortadaki elemanin cagrilmasi
-console.log(items[Math.floor(items.length / 2)]);
-// degisken icindeki bilginin array olup lolmadiginin kontrol edilmesi
-console.log(typeof items); //object
-console.log(Array.isArray(items)); //true
-
-//Hangileri isArray -> true verir?
-console.log("[] : ", Array.isArray([])); // true
-console.log("1 : ", Array.isArray(1)); // false
-console.log("true : ", Array.isArray(true)); // false
+items[10000] = 500;
+console.log(items); //(10001) [5, 20, 30, 60, empty × 9996, 500] // "empty × 9996 BOS OGE ATADI"
