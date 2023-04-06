9a042ca5cd37d8d68263614b9cb6f73218c9eeb0
LEVENT_KOYBASI <leventkoybasi@hotmail.com>
2023-04-05 17:07:51 -0700

    ornek doc eklendi

---
+++ 01 KODLUYORUZ/45 - ODEV 1/app.js
@@ -0,0 +1,61 @@
+/*
+
+const inputBox = document.querySelector('#task');
+const ulDOM = document.getElementById('list');
+
+let toDO = document.querySelector("#formInput")
+toDO.addEventListener('submit', formSubmit)
+
+let addBtn = document.querySelector('#toastWarning')
+let addToast = document.querySelector('#liveToast')
+
+function formSubmit(event) {
+    event.preventDefault()
+}
+
+function setConfig() {
+    list.innerHTML = localStorage.getItem("toDO")
+}
+
+console.log(inputBox);
+function newElement() {
+    if (inputBox.value === "") {
+        toastGoster();
+    } else {
+
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value;
+        ulDOM.appendChild(li)
+        let btn = document.createElement("button")
+        btn.innerHTML = "X"
+        btn.classList.add('close')
+        li.appendChild(btn)
+        addToastF();
+    }
+    inputBox.value = ""
+    addLocalStorage()
+}
+function addLocalStorage() {
+    localStorage.setItem("toDo", list.innerHTML)
+}
+
+ulDOM.addEventListener('click', function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        addLocalStorage()
+    } else if (e.target.tagName === "BUTTON") {
+        e.target.parentElement.remove();
+        addLocalStorage()
+    }
+}, false);
+
+let toastGoster = () => {
+    var show = new bootstrap.Toast(addBtn)
+    show.show()
+}
+let addToastF = () => {
+    var goster = new bootstrap.Toast(addToast)
+    goster.show()
+}
+
+*/
