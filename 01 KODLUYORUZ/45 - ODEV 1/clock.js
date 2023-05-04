9ad8d433ded04cfcceff80c939005e292272fc6e
Lacrimossa <leventkoybasi@hotmail.com>
2023-05-04 16:45:59 -0700

    ODEV 1

--- 01 KODLUYORUZ/45 - ODEV 1/clock.js
+++ 01 KODLUYORUZ/45 - ODEV 1/clock.js
@@ -5,25 +5,30 @@ let fullName = prompt("Lutfen Adinizi Giriniz...");
 myNameDOM.innerHTML = fullName;
 
 let gunArray = [
-	"Pazartesi",
-	"Sali",
-	"Carsamba",
-	"Persembe",
-	"Cuma",
-	"Cumartesi",
-	"Pazar",
+    "Pazartesi",
+    "Sali",
+    "Carsamba",
+    "Persembe",
+    "Cuma",
+    "Cumartesi",
+    "Pazar",
 ];
 
 function showTime() {
-	let time = new Date();
-	let hours = time.getHours();
-	let minutes = time.getMinutes();
-	let seconds = time.getSeconds();
-	// let day = time.getDay();
+    let time = new Date(); //new Date().getHours(); SEKLINDE KULLANILIR!
 
-	myClockDOM.innerHTML = ` ${hours} : ${minutes} : ${seconds} ${
-		gunArray[time.getDay() - 1]
-	}`;
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let day = time.getDay();
+
+    myClockDOM.innerHTML = ` ${hours} : ${minutes} : ${seconds} ${
+        gunArray[day - 1] // new Date().getDay(); 0. INDEX PAZARDIR O YUZDEN -1
+    }`;
+    /*
+    myClockDOM.innerHTML = `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()} : ${
+        gunArray[new Date().getDay() - 1]}`;
+        */
 }
 showTime();
 
