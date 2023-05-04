3451c29bee651ca15669d0485d576fc0f6691e9d
Levent Koybasi <leventkoybasi@hotmail.com>
2023-05-04 16:21:33 -0700

    oev 1

--- 01 KODLUYORUZ/45 - ODEV 1/clock.js
+++ 01 KODLUYORUZ/45 - ODEV 1/clock.js
@@ -5,23 +5,25 @@ let fullName = prompt("Lutfen Adinizi Giriniz...");
 myNameDOM.innerHTML = fullName;
 
 let gunArray = [
-    "Pazartesi",
-    "Sali",
-    "Carsamba",
-    "Persembe",
-    "Cuma",
-    "Cumartesi",
-    "Pazar",
+	"Pazartesi",
+	"Sali",
+	"Carsamba",
+	"Persembe",
+	"Cuma",
+	"Cumartesi",
+	"Pazar",
 ];
 
 function showTime() {
-    let time = new Date();
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-    let day = time.getDay();
+	let time = new Date();
+	let hours = time.getHours();
+	let minutes = time.getMinutes();
+	let seconds = time.getSeconds();
+	// let day = time.getDay();
 
-    myClockDOM.innerHTML = ` ${hours} : ${minutes} : ${seconds} ${day}`;
+	myClockDOM.innerHTML = ` ${hours} : ${minutes} : ${seconds} ${
+		gunArray[time.getDay() - 1]
+	}`;
 }
 showTime();
 
