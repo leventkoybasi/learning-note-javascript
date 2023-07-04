55e282707e95631f6dd4e35978a85cd7336ca508
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-03 18:02:45 -0700

    functions

--- 03 React/05 - Javascript/09 - Fonksiyonlar Ile Calismak/app.js
+++ 03 React/05 - Javascript/09 - Fonksiyonlar Ile Calismak/app.js
@@ -22,3 +22,85 @@ greetings("Levent"); //Merhaba Levent
 //greetings(); //Merhaba undefined
 //greetings(); //Merhaba ISIM YOK
 greetings(); //Merhaba
+
+function greetings_v2(firstName, lastName) {
+  console.log(
+    `Merhaba ${firstName ? firstName : ""} ${lastName ? lastName : ""}`
+  );
+}
+
+greetings_v3("Levent", "Koybasi"); //Merhaba Levent Koybasi
+
+function greetings_v3(firstName = "", lastName = "") {
+  console.log(`Merhaba ${firstName} ${lastName}`);
+}
+greetings_v3("Levent", "Koybasi"); //Merhaba Levent Koybasi
+greetings_v3(); //Merhaba
+
+function multiply(num1, num2) {
+  num1 * num2;
+  //disari herhangi bir bilgi gondermese de iceride bir islem yapiyor
+}
+multiply(10, 20);
+
+function selectHtmlItemAddInformation(element, info) {
+  document.querySelector(element).innerHTML = info;
+}
+selectHtmlItemAddInformation("#app", "Merhaba Dunya");
+
+//return kullanimi
+
+function multiply_with_return(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply_with_return(10, 30)); //300
+//veya
+const result = multiply_with_return(10, 30);
+console.log(result); //300
+
+let user;
+
+function sayHelloToUser() {
+  user = prompt("Adiniz : ");
+  return user;
+}
+
+// user = sayHelloToUser();
+// console.log(user);
+
+/*
+function sayHelloToLorem() {
+  const user = prompt("Adiniz : ");
+  if (user === "Lorem") {
+    console.log("sayHelloToLOrem", user);
+  } else {
+    return sayHelloToLorem();
+  }
+}
+sayHelloToLorem(); // Lorem yazana kadar calisir. */
+
+// Eski Yaptigimiz Bir Calismayi Fonksiyon Olarak Duzenlemek:
+
+function userAccess(userId, userAge) {
+  if (userId.length === 11 && userAge >= 18 && userAge <= 99) {
+    const h1TitleElement = document.querySelector("#app");
+    h1TitleElement.innerHTML = "<em>Siteye Girebilirsiniz..</em>";
+    // h1TitleElement.innerText = '<em>Siteye Girebilirsiniz..</em>'
+    // if (userAge > 99) {
+    //   console.log("Yas Bilgisi Hatali..") // acaba burda olmasi dogr muydu ?
+    // }
+    // console.log("Siteye Girebilirsiniz..")
+  } else if (!userAge <= 99) {
+    console.log("Yas Bilgisi 99dan buyuk olabilir..");
+    // tersini al operatorunu kullandik ;)
+  } else if (userAge > 99) {
+    console.log("Yas Bilgisi 99dan buyuk olabilir..");
+  } else if (userId.length !== 11) {
+    console.log("TC No Bilgisi Dogru Olmayabilir..");
+  } else if (userId.length === 11 && userAge < 18) {
+    console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..");
+  }
+}
+
+userAccess("Lorem Ipsum", 23);
