7c13ae519225a407a2658c1a0850c87ce7c3492d
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-05 18:18:36 -0700

    addeventlistener

---
+++ 03 React/05 - Javascript/13 - Class Eklemek ve Cikartmak/app.js
@@ -0,0 +1,36 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
+const titleElement = document.querySelector("#title");
+console.log(titleElement); //h1#title.title.text-primary
+console.log(titleElement.classList); //DOMTokenList(2) ['title', 'text-primary', value: 'title text-primary']
+
+titleElement.classList.remove("text-primary", "title");
+console.log(titleElement.classList); //DOMTokenList [value: '']
+
+titleElement.classList.add("text-info", "display-1");
+console.log(titleElement.classList); //DOMTokenList(2) ['text-info', 'display-1', value: 'text-info display-1']
+
+titleElement.classList.toggle("bg-primary"); //toggle ekle cikar isleri yapiyor bg-primary class.i varsa cikartir, fakat yoksa ekler. toggle on/off gibi kullanabiliriz
+
+const btn = document.querySelector("#hideJavascript");
+btn.addEventListener("click", toggleTitle);
+function toggleTitle() {
+  const h1Element = document.querySelector("#title");
+  // h1Element.classList.toggle("d-none");
+  h1Element.classList.toggle("invisible");
+}
+
+//Yerinde function yazmak
+
+const btn2 = document.querySelector("#hideText");
+btn2.addEventListener("click", function () {
+  const pElement = document.querySelector("#info");
+  pElement.classList.toggle("invisible");
+});
+
+//Arrow Function Ile yazmak
+const btn3 = document.querySelector("#hideImg");
+btn3.addEventListener("click", () => {
+  const imgElement = document.querySelector("#image");
+  imgElement.classList.toggle("invisible");
+});
