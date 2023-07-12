ac34b989a0914645a2b0bdc1a5a6bd4ce0529167
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-11 17:54:26 -0700

    Fetch API Kullanimi

--- 03 React/05 - Javascript/22 - Fetch API Kullanimi/app.js
+++ 03 React/05 - Javascript/22 - Fetch API Kullanimi/app.js
@@ -2,6 +2,36 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 // https://jsonplaceholder.typicode.com/
 
+fetch()
+  .then((response) => response.json())
+  .then((response) => console.log(response)); //Yapi BU Sekilde //
+
+/*
+  İlk olarak, fetch() fonksiyonunu çağırıyoruz. Bu, bir ağ isteği yapmamızı ve istenen kaynağın yanıtını almak için kullanmamızı sağlar. İstek yapılandırmasını içeren bir argüman alır (URL gibi) ve bir Promise döndürür.
+
+fetch() fonksiyonu Promise döndürdüğü için, ardından .then() yöntemini kullanıyoruz. Bu yöntem, Promise'in çözüldüğünde çalışacak bir geri çağırma fonksiyonu kabul eder. İlk .then() bloğunda, yanıt nesnesini alırız.
+
+İkinci .then() bloğunda, yanıt nesnesini JSON formatına dönüştürüyoruz. Bu, yanıt verisinin bir JavaScript nesnesine dönüştürülmesini sağlar.
+
+Son olarak, JSON verisini konsola yazdırmak için console.log() fonksiyonunu kullanıyoruz. Bu, yanıtı tarayıcının geliştirici aracındaki konsol panelinde görüntülememizi sağlar.
+
+Bu yapı, bir ağ isteği yapar, istenen kaynağın yanıtını alır, yanıtı JSON formatına dönüştürür ve sonucu konsola yazdırır. Bu, tipik olarak API'lerle çalışırken sıkça kullanılan bir yapıdır.
+----------------------------------------------------------------
+  */
+
+/*
+İlk olarak, fetch() fonksiyonunu çağırıyoruz. Bu, bir ağ isteği yapmamızı ve istenen kaynağın yanıtını almak için kullanmamızı sağlar. İstek yapılandırmasını içeren bir argüman alır (URL gibi) ve bir Promise döndürür.
+
+fetch() fonksiyonu Promise döndürdüğü için, ardından .then() yöntemini kullanıyoruz. Bu yöntem, Promise'in çözüldüğünde çalışacak bir geri çağırma fonksiyonu kabul eder. İlk .then() bloğunda, yanıt nesnesini alırız.
+
+İkinci .then() bloğunda, yanıt nesnesini JSON formatına dönüştürüyoruz. Bu, yanıt verisinin bir JavaScript nesnesine dönüştürülmesini sağlar.
+
+Son olarak, JSON verisini konsola yazdırmak için console.log() fonksiyonunu kullanıyoruz. Bu, yanıtı tarayıcının geliştirici aracındaki konsol panelinde görüntülememizi sağlar.
+
+Bu yapı, bir ağ isteği yapar, istenen kaynağın yanıtını alır, yanıtı JSON formatına dönüştürür ve sonucu konsola yazdırır. Bu, tipik olarak API'lerle çalışırken sıkça kullanılan bir yapıdır.
+----------------------------------------------------------------
+*/
+
 const JSONPlaceHolderApi = "https://jsonplaceholder.typicode.com/todos";
 
 /*
@@ -25,6 +55,24 @@ fetch(JSONPlaceHolderApi)
       liElement.innerHTML = item.title;
       createdUlElement.append(liElement);
     });
+  });
+
+const JSONPlaceHolderApi2 = "https://jsonplaceholder.typicode.com/todos";
+const app2 = document.querySelector("#app2");
+const createdUlElement2 = document.createElement("ul");
+app.append(createdUlElement2);
+// _____________________________________________________
+
+fetch(JSONPlaceHolderApi2)
+  .then((res) => res.json())
+  .then((json) => {
+    const listItems2 = json.map((item) => {
+      const liElement2 = document.createElement("li");
+      liElement2.innerHTML = item.id;
+      return liElement2;
+    });
+
+    createdUlElement2.append(...listItems2);
   });
 
 fetch("app.json")
