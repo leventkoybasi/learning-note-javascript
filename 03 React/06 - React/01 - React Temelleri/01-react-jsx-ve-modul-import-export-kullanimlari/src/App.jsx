38c63e0eac9372df1cf714baaa01e6773d74cee8
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-13 18:20:46 -0700

    react5 eklendi

--- 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/App.jsx
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // 1: return ederken HTML yapisini return edebilirsin
+  // 2: return ile sadece bir parent element gonderebilirsin
+  // 3: fragment kullanilabilir <> ... </> or <<React.Fragment>> kullanilacaksa react import edilmeli.
+  // 4: Bir etiketin mutlaka bitisi olmalisir. (<etiket>..<etiket/>)
+
+  const userName = "Levent";
+  const users = ["Lorem", "Ipsum", "Dolor"];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Merhaba</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Div Icerisindeki Bilgi</div>
+      <h1>
+        Merhaba (2 + 2 String Bilgi) {2 + 2} {userName}
+      </h1>
+      <h2>Kullanicilarin Sayisi : {users.length}</h2>
     </>
   );
+  //veya
+  // return (
+  //   <React.Fragment>
+  //     <div>Div Icerisindeki Bilgi</div>
+  //     <h1>Merhaba</h1>;
+  //   </React.Fragment>
+  // );
 }
-
 export default App;
+//https://react.dev/learn/writing-markup-with-jsx
--- 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/main.jsx
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//ReactDOM.createRoot(document.getElementById("root")). yerine
+const root = document.querySelector("#root");
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
