af82f6510890c736bb36fa3b2d380d4b61bb246f
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-17 18:02:59 -0700

    use State bil;gisi

--- 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/AppWithState.jsx
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/AppWithState.jsx
@@ -1,14 +1,27 @@
+//UseState => durum bil;gisi ve durum bilgisini degistirecek fonksiyon
+
+import { useState } from "react";
+
 export default function AppWithState() {
-  const counter = 0;
+  // let counter = 0;
+  function exampleFunc(default) {
+    let item = default
+    function changeItem(default) {
+      
+    }
+    return [item]
+  }
 
   function handleClick() {
     console.log("HandleClick Buttonuna Tiklandi...");
+    counter += 1;
+    console.log(counter);
   }
 
   return (
     <>
       <h1>State Konusunu Burada Isliyoruz...</h1>
-      <h2>Counter : </h2>
+      <h2>Counter : {counter}</h2>
       <button onClick={() => console.log("OnClick Buttona Tiklandi...")}>
         Counter Bilgisini Arttir
       </button>
