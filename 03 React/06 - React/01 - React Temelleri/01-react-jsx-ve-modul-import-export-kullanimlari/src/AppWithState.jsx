ef204b764342e7c8e9c9c269dcf6fa56de23ca6d
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-17 17:48:37 -0700

    handleClick

--- 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/AppWithState.jsx
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/AppWithState.jsx
@@ -1,7 +1,18 @@
 export default function AppWithState() {
+  const counter = 0;
+
+  function handleClick() {
+    console.log("HandleClick Buttonuna Tiklandi...");
+  }
+
   return (
     <>
       <h1>State Konusunu Burada Isliyoruz...</h1>
+      <h2>Counter : </h2>
+      <button onClick={() => console.log("OnClick Buttona Tiklandi...")}>
+        Counter Bilgisini Arttir
+      </button>
+      <button onClick={handleClick}>Counter Bilgisini Arttir</button>
     </>
   );
 }
