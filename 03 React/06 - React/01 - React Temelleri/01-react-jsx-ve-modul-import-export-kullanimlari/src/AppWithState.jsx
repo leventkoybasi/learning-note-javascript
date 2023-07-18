235924cb861e0f4dd5cb9227b17685480cd8b426
Lacrimossa <leventkoybasi@hotmail.com>
2023-07-17 17:36:18 -0700

    state konusunu biurada isliyoruz

---
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/AppWithState.jsx
@@ -0,0 +1,7 @@
+export default function AppWithState() {
+  return (
+    <>
+      <h1>State Konusunu Burada Isliyoruz...</h1>
+    </>
+  );
+}
--- 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/main.jsx
+++ 03 React/06 - React/01 - React Temelleri/01-react-jsx-ve-modul-import-export-kullanimlari/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import AppWithState from "./AppWithState";
 //ReactDOM.createRoot(document.getElementById("root")). yerine
 const root = document.querySelector("#root");
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <AppWithState />
   </React.StrictMode>
 );
