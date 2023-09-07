6cbbe249f609ccf7fa64da230acf3aa03be8a208
Lacrimossa <leventkoybasi@hotmail.com>
2023-09-06 17:53:32 -0700

    add to new items

--- 01 Patika Dev/02 INTERMEDIATE PATHWAY/01 REACT/01-State/src/App.jsx
+++ 01 Patika Dev/02 INTERMEDIATE PATHWAY/01 REACT/01-State/src/App.jsx
@@ -2,10 +2,53 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Levent");
+  const [age, setAge] = useState("");
 
   return (
     <>
-      <div>ilk proje</div>
+      <div>
+        <h1>COUNT : {count}</h1>
+        <button
+          className="btn btn-success px-4 mx-2"
+          onClick={() => setCount((prevCount) => prevCount + 1)}
+        >
+          +
+        </button>
+        <button
+          className="btn btn-danger px-4 mx-2"
+          onClick={() =>
+            setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0))
+          }
+        >
+          -
+        </button>
+      </div>
+      <hr />
+      <div>
+        <h1>name : {name}</h1>
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            setName(`${name === "KOYBASI" ? "Levent" : "KOYBASI"}`)
+          }
+        >
+          AD DEGISTIR
+        </button>
+      </div>
+      <hr />
+      <div>
+        <h1>age : {age}</h1>
+        <button
+          className="btn btn-primary"
+          onClick={() => setAge(`${age === "" ? 38 : ""}`)}
+        >
+          {/* {`${age === "" ? "SHOW AGE" : "HIDE AGE"}`} */}
+          {age === "" ? "SHOW AGE" : "HIDE AGE"}
+        </button>
+      </div>
+      <hr />
+      <div></div>
     </>
   );
 }
