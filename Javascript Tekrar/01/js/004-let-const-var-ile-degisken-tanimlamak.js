98a93c48784d7bde825a09adbf934fa0b6ec385c
Levent KOYBASI <johndoe@example.comleventkoybasi@hotmail.com>
2023-11-02 18:03:11 -0700

    let const

---
+++ Javascript Tekrar/01/js/004-let-const-var-ile-degisken-tanimlamak.js
@@ -0,0 +1,20 @@
+let user;
+console.log(user);
+
+// let user = "Levent";
+// console.log(user);
+
+user = "Levent";
+console.log(user);
+
+user = "ADMIN";
+console.log(user);
+
+const PASSWORD = "123456789";
+console.log("Sifre :", PASSWORD);
+
+// PASSWORD = 1234;
+// console.log("Sifre :", PASSWORD);
+// const ile bir kez tanimlanir...
+
+//185 8. dakikada kaldim.
