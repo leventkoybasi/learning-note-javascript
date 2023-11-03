1eb7816d4e92d826337b078099ab5c657043ee89
Levent KOYBASI <johndoe@example.comleventkoybasi@hotmail.com>
2023-11-02 17:26:34 -0700

    003 variables

---
+++ Javascript Tekrar/01/js/002 console.js
@@ -0,0 +1,15 @@
+console.log(console);
+
+console.count("Count ?");
+console.count("Count ?");
+
+console.error("Hata bilgisinin dikkat cekmesi icin bu sekilde yazilabilir.");
+
+const user = {
+  firstName: "Levent",
+  lastName: "KOYBASI",
+  age: 35,
+};
+console.log("log :", user);
+console.table("table :");
+console.table(user);
---
+++ Javascript Tekrar/01/js/003 variable.js
@@ -0,0 +1,4 @@
+let API_KEY = "ABHDJKSLJKNMNDFMSHNFJDIKSFGJLDSKFJDKSLF8DS7F834HFJDK";
+console.log(API_KEY);
+let info = "Levent'in Bilgisayari";
+console.log(info);
