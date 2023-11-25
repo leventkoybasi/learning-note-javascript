99e197edeed413c002c8352cf230e850610d07c6
leventkoybasi <leventkoybasi@hotmail.com>
2023-11-24 22:21:19 -0800

    karsilastirma operatorleri

--- Javascript Tekrar/01/js/016-karsilastirma-operatorleri-ile-calismak.js
+++ Javascript Tekrar/01/js/016-karsilastirma-operatorleri-ile-calismak.js
@@ -1,2 +1,38 @@
 const sysPassword = 123456;
-let levent;
+
+// const userPasswordPrompt = prompt("Lutfen Kullanici Sifrenizi Giriniz...");
+
+// console.log("User Prompt :", userPasswordPrompt);
+
+const userPassword = "123456";
+
+console.log(sysPassword == userPassword);
+console.log(typeof sysPassword == typeof userPassword);
+
+const superUser = "admin";
+console.log("Normal Kullanici Mi? :", superUser != "Levent"); // true
+
+const year = 2000;
+
+console.log("Year 2000'den Buyuk Mu?", 2010 > year); //true
+console.log("Year 2000'den Buyuk Mu?", 1980 > year); //true
+console.log("Year 2000'den Kucuk Mu?", 2010 < year); //false
+
+const ageLimit = 18;
+const userAge = 18;
+
+console.log("Kullanici Sisteme Girebilir mi?", userAge > ageLimit); //false
+console.log("Kullanici Sisteme Girebilir mi?", userAge >= ageLimit); //true
+
+// > < >= <=
+
+const userName = "Mine";
+const userPassword2 = "123456";
+console.log(
+  "User bilgisi 3 karakterden buyuk mu ve password bilghisi var mi? ",
+  userName.length > 3 && userPassword2
+);
+console.log(
+  "User bilgisi 3 karakterden buyuk mu veya password bilghisi var mi? ",
+  userName.length > 3 || userPassword2
+);
